a9d9a59a3914539701ec1a2155d278c14282ba38
Khavin Senthilvel <senthilvelkhavin@gmail.com>
2024-10-11 21:22:46 -0400

    Update wsprojectmi.py

--- wsprojectmi.py
+++ wsprojectmi.py
@@ -1,46 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import lxml
 import bs4
 import requests
-
-
-# In[ ]:
-
-
-# request = requests.get('https://books.toscrape.com/catalogue/page-1.html')
-
-
-# In[ ]:
-
-
-# wscrape = bs4.BeautifulSoup(request.text, 'lxml')
-
-
-# In[ ]:
-
-
-# for i in range(len(wscrape.select('.image_container'))):
-#     print(wscrape.select('.image_container')[i].select('img')[0]['alt'])
-
-
-# In[ ]:
-
-
-# pageno = wscrape.select('.current')
-
-
-# In[ ]:
-
-
-# pageno[0].text
-
-
-# In[ ]:
 
 
 sf = int(input('How many pages of the shop would you like to view the titles?' + ' '))
@@ -52,7 +15,7 @@ for pg in range(sf + 1):
         print(wscrape.select('.image_container')[i].select('img')[0]['alt']) 
 
 
-# In[ ]:
+
 
 
 
