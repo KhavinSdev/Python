66986cea2fdd457959e5a196a17d5040e608d544
Khavin Senthilvel <senthilvelkhavin@gmail.com>
2024-10-11 22:48:55 -0400

    Update chattybuffy (1).py

--- chattybuffy (1).py
+++ chattybuffy (1).py
@@ -11,13 +11,13 @@ pairs = [
     ['my name is (.*)', ['hi %1']],
     ['(hi|hello|hey|vanakum|vannakum|namaste|bonjour|salut|hola|hylo|hey there)', ['hi there!','what up','Greetings','haaaaay!','hi..I guess','Oh Hi!']],
     ['(.*) created you?',['Khavin created me.']],
-    ['(.*)(location|city|country)', ['I reside in India in the state of TamilNadu']],
+    ['(.*)(location|city|country)', ['I reside in The World in the state of The world']],
     ['(.*) your name?',["my name is buffy-bot."]],
     ['(.*) (date)',[now.strftime("%Y-%m-%d")]],
     ['(.*) (time)',[now.strftime('%I:%M:%S %p')]],
     ['(bye|Bye|Good bye|good bye|salut|au revoir|Nandri|Namaste)',['bye, have a nice day or night!']],
-    ['(what|What|Which|which) (is|are) (your|Your) (favourite|Preferred|beloved|liked|) candy', ['I like cotton candy aswell as fruit flavoured candies!']],
-    ['(.*) joke',['did you hear about the movie constipation.... Never mind,It never came out!','How do you make holy water? You boil the hell out of it.','Did you know the first French fries weren\'t actually cooked in France? They were cooked in Greece.','What is the least spoken language in the world? Sign language','Don\'t trust atoms. They make up everything!']]
+    ['(what|What|Which|which) (is|are) (your|Your) (favourite|Preferred|beloved|liked|) candy', ['I like cotton candy as well as fruit flavoured candies!']],
+    ['(.*) joke',['did you hear about the movie constipation.... Never mind,It never came out!','Did you know the first French fries weren\'t actually cooked in France? They were cooked in Greece.','What is the least spoken language in the world? Sign language','Don\'t trust atoms. They make up everything!']]
 ]
 
 reflections
